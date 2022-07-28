cd6b1d35317061f1c951ffa1a2a9a84b6b431138
PatrickA <patrick@agut.cat>
2022-07-28 19:17:24 +0000

    Added question 3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Loop exercises");
@@ -5,6 +7,8 @@ public class Main {
         question1();
         System.out.println("Question 2:");
         question2();
+        System.out.println("Question 3:");
+        question3();
     }
 
     public static void question1(){
@@ -19,5 +23,15 @@ public class Main {
             sum += i;
         }
         System.out.println("Result: "+sum);
+    }
+
+    public static void question3(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter a positive natural number");
+        int userNumber = Integer.parseInt(reader.nextLine());
+        for (int i = 1; i<=10;i++){
+            System.out.println(userNumber+"*"+i+"="+userNumber*i);
+        }
+
     }
 }
