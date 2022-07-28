722d05c7718f1160a39d790ed8915df5717434d4
PatrickA <patrick@agut.cat>
2022-07-28 18:54:07 +0000

    Created project and added question 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Loop exercises");
+        question1();
+    }
+
+    public static void question1(){
+        for (int i = 1; i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
